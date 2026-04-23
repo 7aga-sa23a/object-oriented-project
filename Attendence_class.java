aef624bdef0aa7965b13017591e4cb8335641ef6
shroukatef7926-gif <shroukatef098@gmail.com>
2026-04-23 19:44:22 +0200

    feat: attendance

---
+++ Attendence_class.java
@@ -0,0 +1,44 @@
+class Attendance {
+    String studentName;
+    int attendedLectures;
+    int totalLectures;
+
+    // Constructor
+    Attendance(String name, int attended, int total) {
+        studentName = name;
+        attendedLectures = attended;
+        totalLectures = total;
+    }
+
+    // حساب النسبة
+    double calculatePercentage() {
+        return ((double) attendedLectures / totalLectures) * 100;
+    }
+
+    // تحديد التقييم
+    String getGrade() {
+        double percent = calculatePercentage();
+
+        if (percent >= 90)
+            return "A";
+        else if (percent >= 80)
+            return "B";
+        else if (percent >= 70)
+            return "C";
+        else if (percent >= 60)
+            return "D";
+        else
+            return "F";
+    }
+
+    // عرض بيانات الطالب
+    void display() {
+        double percent = calculatePercentage();
+
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Attendance: " + attendedLectures + "/" + totalLectures);
+        System.out.printf("Percentage: %.2f%%\n", percent);
+        System.out.println("Grade: " + getGrade());
+        System.out.println("----------------------------");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Attendance s1 = new Attendance("Ahmed", 18, 20);
+        Attendance s2 = new Attendance("Sara", 15, 20);
+        Attendance s3 = new Attendance("Mona", 10, 20);
+
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}
